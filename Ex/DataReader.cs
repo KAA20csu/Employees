0cd6a62373b91193ed77c35e55ab09716d2d2435
Evgenii Klimov <vikkes@yandex.ru>
2021-04-01 18:03:57 +0700

    add comment

--- Ex/DataReader.cs
+++ Ex/DataReader.cs
@@ -7,6 +7,12 @@ namespace Ex
 	{
 		public static List<Person> person = new List<Person>();
 
+		/// <summary>
+		/// В данном случае (да и чаще всего) статический класс это плохо. Скажем так, в
+		/// будущем возникнут проблемы с потокобезопасностью, а так же, данная коллекция
+		/// хранится на протяжении всего времени выполнения приложения. Но зачем,
+		/// используется только раз
+		/// </summary>
 		public static string[] information { get; } = File.ReadAllLines("tsk.txt");
 
 		public static void GetPerson()
