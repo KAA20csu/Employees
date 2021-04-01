0490814481069297fec19fe0ac327e482d720db8
Evgenii Klimov <vikkes@yandex.ru>
2021-04-01 18:15:08 +0700

    rename informations to information. DataReader review

--- Ex/DataReader.cs
+++ Ex/DataReader.cs
@@ -1,26 +1,14 @@
-using System.Collections.Generic;
 using System.IO;
 
 namespace Ex
 {
-	/// <summary>
-	/// Уходим от статики
-	/// </summary>
 	internal class DataReader
 	{
-		public static List<Person> Persons { get; }  = new List<Person>();
-
-		/// <summary>
-		/// Всё это можно сделать в методе, и не хранить вечно в статическом свойсте
-		/// </summary>
-		public static string[] Informations { get; } = File.ReadAllLines("tsk.txt");
-
-		public static void GetPerson()
+		public string[] GetPerson()
 		{
-			foreach (var employee in Informations)
-			{
-				Persons.Add(new Person(employee.Split(';'), new WorkPlace(employee.Split(';')[1])));
-			}
+			var information = File.ReadAllLines("tsk.txt");
+
+			return information;
 		}
 	}
 }
