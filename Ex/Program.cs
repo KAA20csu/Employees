886baf38e14643303f78828fe30b4c586d7a960a
Evgenii Klimov <vikkes@yandex.ru>
2021-04-01 19:23:43 +0700

    add comments

--- Ex/Program.cs
+++ Ex/Program.cs
@@ -1,11 +1,13 @@
 namespace Ex
 {
+	/// <summary>
+	/// теперь осталось только вызвать реализованное
+	/// </summary>
 	internal class Program
 	{
 		private static void Main(string[] args)
 		{
-			DataReader.GetFileData();
-			WorkPlace.GetWorkPlaceInfo();
+			
 		}
 	}
 }
