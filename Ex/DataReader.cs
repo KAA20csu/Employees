0cf8f6f1ba88af77a1714a672f969b421caf6242
Evgenii Klimov <vikkes@yandex.ru>
2021-04-01 18:12:15 +0700

    add comments

--- Ex/DataReader.cs
+++ Ex/DataReader.cs
@@ -3,10 +3,16 @@ using System.IO;
 
 namespace Ex
 {
+	/// <summary>
+	/// Уходим от статики
+	/// </summary>
 	internal class DataReader
 	{
 		public static List<Person> Persons { get; }  = new List<Person>();
 
+		/// <summary>
+		/// Всё это можно сделать в методе, и не хранить вечно в статическом свойсте
+		/// </summary>
 		public static string[] Informations { get; } = File.ReadAllLines("tsk.txt");
 
 		public static void GetPerson()
