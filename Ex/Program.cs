ae4ef767089021ec00c8574244475f6bd5b9af17
Evgenii Klimov <vikkes@yandex.ru>
2021-04-01 17:59:09 +0700

    code style review

--- Ex/Program.cs
+++ Ex/Program.cs
@@ -5,102 +5,113 @@ using System.Linq;
 
 namespace Ex
 {
-	/// <summary>
-	/// Немного исправлю стиль кода
-	/// </summary>
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DataReader.GetPerson();
-            WorkPlace.GetWorkPlaceInfo();
-            
-        }
-    }
-    class DataReader
-    {
-        public static string[] information { get; private set; } = File.ReadAllLines("tsk.txt");
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			DataReader.GetPerson();
+			WorkPlace.GetWorkPlaceInfo();
+		}
+	}
 
-        public static List<Person> person = new List<Person>();
+	internal class DataReader
+	{
+		public static List<Person> person = new List<Person>();
 
-        public static void GetPerson()
-        {
-            foreach(var employee in information)
-            {
-                person.Add(new Person(employee.Split(';'), new WorkPlace(employee.Split(';')[1])));
-            }
-        }
-    }
-    class Person
-    {
-        public string Name { get; private set; }
-        public WorkPlace Work { get; private set; }
-        public int Salary { get; private set; }
-        public bool IsHead { get; private set; } = false;
+		public static string[] information { get; } = File.ReadAllLines("tsk.txt");
 
-        public Person(string[] inforamtion, WorkPlace work)
-        {
-            Name = inforamtion[0];
-            Work = work;
-            Salary = int.Parse(inforamtion[2]);
-            if (inforamtion.Length == 4)
-                IsHead = Convert.ToBoolean(inforamtion[3]);
-            else IsHead = false;
-        }
-    }
-    class WorkPlace
-    {
-        public string workName { get; private set; }
-        
-        public WorkPlace(string work)
-        {
-            workName = work;
-        }
+		public static void GetPerson()
+		{
+			foreach (var employee in information)
+			{
+				person.Add(new Person(employee.Split(';'), new WorkPlace(employee.Split(';')[1])));
+			}
+		}
+	}
 
-        public static void GetWorkPlaceInfo()
-        {
-            var groupOfemployees = DataReader.person.GroupBy(e => e.Work.workName, (key, g) => 
-            new { Key = key, Value = g.Count(f => f.IsHead == true) }).OrderBy(c => c.Key);
-            foreach (var employee in groupOfemployees)
-            {
-                if (employee.Value < 1 || employee.Value > 2)
-                    throw new Exception("Ошибка!");
-            }
-            Salaries.GetSalaries();
-            Salaries.GetTheRichestHead();
-        }
-    }
-    class Salaries
-    {
-        public static void GetSalaries()
-        {
+	internal class Person
+	{
+		public Person(string[] inforamtion, WorkPlace work)
+		{
+			Name = inforamtion[0];
+			Work = work;
+			Salary = int.Parse(inforamtion[2]);
 
-            var groupOfEmployees = DataReader.person.GroupBy(e => e.Work.workName,
-                (key, g) => new { Key = key, Value = g.Where(g => g.IsHead == false) }
-                );
-            foreach (var employee in groupOfEmployees)
-            {
-                double salary = employee.Value.Average(f => f.Salary);
-                Console.WriteLine($"Средняя зарплата по {employee.Key}: " + salary);
-            }
-        }
-        public static void GetTheRichestHead()
-        {
-            var groupOfHeads = DataReader.person.GroupBy(e => e.Work.workName,
-                (key, g) => new { Key = key, Value = g.Where(g => g.IsHead == true) }
-                );
-            List<double> headSalaries = new List<double>();
-            foreach (var head in groupOfHeads)
-            {
-                headSalaries.Add(head.Value.Max(f => f.Salary));
-            }
-            var richest = headSalaries.Max();
-            Console.WriteLine($"Cамая высокая зарплата руководителя: " + richest);
-        }
-    }
-    
+			if (inforamtion.Length == 4)
+			{
+				IsHead = Convert.ToBoolean(inforamtion[3]);
+			} else
+			{
+				IsHead = false;
+			}
+		}
+
+		public string Name { get; }
+
+		public WorkPlace Work { get; }
+
+		public int Salary { get; }
+
+		public bool IsHead { get; }
+	}
+
+	internal class WorkPlace
+	{
+		public WorkPlace(string work)
+		{
+			workName = work;
+		}
+
+		public string workName { get; }
+
+		public static void GetWorkPlaceInfo()
+		{
+			var groupOfemployees = DataReader.person.GroupBy(e => e.Work.workName,
+					(key, g) =>
+						new { Key = key, Value = g.Count(f => f.IsHead) })
+				.OrderBy(c => c.Key);
+
+			foreach (var employee in groupOfemployees)
+			{
+				if (employee.Value < 1 || employee.Value > 2)
+				{
+					throw new Exception("Ошибка!");
+				}
+			}
+
+			Salaries.GetSalaries();
+			Salaries.GetTheRichestHead();
+		}
+	}
+
+	internal class Salaries
+	{
+		public static void GetSalaries()
+		{
+			var groupOfEmployees = DataReader.person.GroupBy(e => e.Work.workName,
+				(key, g) => new { Key = key, Value = g.Where(g => g.IsHead == false) });
+
+			foreach (var employee in groupOfEmployees)
+			{
+				var salary = employee.Value.Average(f => f.Salary);
+				Console.WriteLine($"Средняя зарплата по {employee.Key}: " + salary);
+			}
+		}
+
+		public static void GetTheRichestHead()
+		{
+			var groupOfHeads = DataReader.person.GroupBy(e => e.Work.workName,
+				(key, g) => new { Key = key, Value = g.Where(g => g.IsHead) });
+
+			var headSalaries = new List<double>();
+
+			foreach (var head in groupOfHeads)
+			{
+				headSalaries.Add(head.Value.Max(f => f.Salary));
+			}
+
+			var richest = headSalaries.Max();
+			Console.WriteLine("Cамая высокая зарплата руководителя: " + richest);
+		}
+	}
 }
-                
-            
-    
-
