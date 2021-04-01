82a7bb110d32c350242e1bc9db61d9e51702a9ff
Evgenii Klimov <vikkes@yandex.ru>
2021-04-01 19:12:43 +0700

    add data validation

---
+++ Ex/Validations/FileDataValidation.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex.Validations
+{
+	internal class FileDataValidation
+	{
+		public static void Validate(IList<Person> persons)
+		{
+			foreach (var workGroup in persons.GroupBy(person => person.Work))
+			{
+				var heads = workGroup.Count(v => v.IsHead);
+
+				if (heads < 1 || heads > 2)
+				{
+					throw new Exception("Ошибка!");
+				}
+			}
+		}
+	}
+}
